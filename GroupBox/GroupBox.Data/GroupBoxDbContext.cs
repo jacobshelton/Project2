aa71526998f6f17d00f47e495af8afc52fba5291
jacob shelton <jacobshelton2@outlook.com>
2019-09-09 10:13:22 -0500

    attempted many to many (does not work yet)

---
+++ GroupBox/GroupBox.Data/GroupBoxDbContext.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using GroupBox.Domain.Models;
+
+namespace GroupBox.Data
+{
+  public class GroupBoxDbContext : DbContext
+  {
+    public DbSet<User> Users { get; set; }
+    public DbSet<Password> Passwords { get; set; }
+    public DbSet<Group> Groups { get; set; }
+    public DbSet<Post> Posts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder builder)
+    {
+      builder.UseSqlServer("server=localhost;initial catalog=GroupBoxDb;user id=sa;password=Password12345;");
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<User>().HasKey(u => u.ID);
+        builder.Entity<User>().HasIndex(u => u.UserName).IsUnique();
+
+        builder.Entity<Password>().HasKey(p => p.ID);
+        builder.Entity<Password>().HasIndex(p => p.Hash).IsUnique();
+
+        builder.Entity<Group>().HasKey(g => g.ID);
+        builder.Entity<Group>().HasIndex(g => g.Name).IsUnique();
+
+        builder.Entity<Post>().HasKey(p => p.ID);
+
+    }
+  }
+}
