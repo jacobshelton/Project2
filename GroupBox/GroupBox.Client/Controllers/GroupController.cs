400fcc07557b699837bff3951b12d4a40c807b03
jacob shelton <jacobshelton2@outlook.com>
2019-09-14 21:50:04 -0500

    updated Group views and GroupController

--- GroupBox/GroupBox.Client/Controllers/GroupController.cs
+++ GroupBox/GroupBox.Client/Controllers/GroupController.cs
@@ -1,21 +1,26 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using GroupBox.Client.Models;
 using GroupBox.Domain.Models;
+using GroupBox.Data;
+using System.Linq;
 
 namespace GroupBox.Client.Controllers
 {
     public class GroupController : Controller
     {
+        private GroupBoxDbContext db = new GroupBoxDbContext();
+
         [HttpGet]
         public IActionResult Create()
         {
-        return View();
+            return View();
         } 
 
         [HttpPost]
         public IActionResult Create(Group group)
         {
+            db.Groups.Add(group);
+            db.SaveChanges();
+
             return View();
         }
 
@@ -23,7 +28,7 @@ namespace GroupBox.Client.Controllers
         [HttpGet]
         public IActionResult AllGroups()
         {
-            return View();
+            return View(db.Groups.ToList());
         }
 
         [HttpPost]
